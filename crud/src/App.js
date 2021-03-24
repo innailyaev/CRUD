f9a101a4e98a4292106383954672779b02111596
Inna Ilyaev <inna4ilyaev@gmail.com>
2021-03-24 17:00:02 +0200

    24/03/21#1

---
+++ crud/src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import AvatarsApi from "./Components/AvatarsApi";
+
+function App() {
+  return (
+    <div className="App">
+        <AvatarsApi/>
+    </div>
+  );
+}
+
+export default App;
